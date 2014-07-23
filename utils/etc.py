e62133a1706c96b4904cfcb13558f4fbcdd53ba2
Daegil Kim <daegil@gmail.com>
2014-07-23 20:23:12 +0900

    to_unicode_or_bust() add

--- utils/etc.py
+++ utils/etc.py
@@ -18,7 +18,17 @@ def timestamp_to_datetime(ts):
 def datetime_to_timestamp(dt):
     return int(time.mktime(dt.timetuple()))
 
+# func. for 'Decode early' (ref : http://farmdev.com/talks/unicode/)
+def to_unicode_or_bust(obj, encoding='utf-8'):
+    if isinstance(obj, basestring):
+        if not isinstance(obj, unicode):
+            obj = unicode(obj, encoding)
+    return obj
+
 if __name__ == '__main__':
     dt = datetime.now()
     ts = datetime_to_timestamp(dt)
-    print ts, timestamp_to_datetime(ts)
+    print ts, timestamp_to_datetime(1401702950)
+    obj = to_unicode_or_bust('가나다')
+    print obj
+    print type(obj)
